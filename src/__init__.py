fcdf788d58eb4efd773ed6d3e340dc9abcda46c1
amantomarwoway <amantomar748@gmail.com>
2026-09-12 15:46:47 +0530

    Create __init__.py for src package
    Add __init__.py to src directory for package initialization.

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+# src/__init__.py - DETAILED FIXED - empty but required for Python package
+# Is file ko src folder me rakh - runner pe src imports kaam karenge
+"""
+GOD LEVEL BOT - 5 LEGS
+src package init - required for imports
+"""

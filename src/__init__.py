d693e1bffb34ec7d905e15aa05ab812359731b88
amantomarwoway <amantomar748@gmail.com>
2026-09-15 11:49:37 +0530

    Delete src directory

--- src/__init__.py
+++
@@ -1,6 +0,0 @@
-# src/__init__.py - DETAILED FIXED - empty but required for Python package
-# Is file ko src folder me rakh - runner pe src imports kaam karenge
-"""
-GOD LEVEL BOT - 5 LEGS
-src package init - required for imports
-"""
